24b2e2ad62bb062b9a02a3951f48451e55673827
chaupb-06 <phambaochauk6@gmail.com>
2026-01-17 10:28:34 +0700

    Uncomment SQL commands for database and table setup

--- Sql_Session06_01.sql
+++ Sql_Session06_01.sql
@@ -15,9 +15,11 @@ insert into Product (name, category, price, stock) values
 ('Bàn học gỗ', 'Furniture', 2500000.00, 20),
 ('Ghế xoay văn phòng', 'Furniture', 1800000.00, 15),
 ('Tai nghe Bluetooth', 'Accessories', 1200000.00, 30);
-
+--Hiển thị danh sách toàn bộ sản phẩm
 select * from Product;
+--Hiển thị 3 sản phẩm có giá cao nhất
 select * from Product order by price desc limit 3;
+--Hiển thị các sản phẩm thuộc danh mục “Điện tử” có giá nhỏ hơn 10,000,000
 select * from Product where category = 'Electronics' and price < 10000000;
+--Sắp xếp sản phẩm theo số lượng tồn kho tăng dần
 select * from Product order by stock asc;
-
